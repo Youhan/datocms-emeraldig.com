688295bde6845662dcff14292cfcd0a8e2c78e58
Alireza Jahandideh <ar.jahandideh@gmail.com>
2021-01-02 13:29:46 -0600

    update

--- pages/index.vue
+++ pages/index.vue
@@ -46,11 +46,14 @@
           <div class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
             <div class="text-center">
               <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span class="block">Materials to enrich your</span>
-                <span class="block text-indigo-600">Cosmetics business</span>
+                <span class="block">Website under maintenance</span>
+                <!-- <span class="block text-indigo-600">Cosmetics business</span> -->
               </h1>
               <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                Ever since it was founded in 2005, Emerald has been conducting the import and marketing of chemicals, packaging and providing formulation support, and consultancy.
+                In the meanwhile please contact: +90 536 043 73 54 or +1 347 991 14 76
+              </p>
+              <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                Kucukbakkal koy mah, sel vili sk, no. 4, iç kapi no.48, atasehir, istanbul
               </p>
             </div>
           </div>
@@ -66,7 +69,7 @@
         </div>
       </div>
       
-      <div class="bg-gray-800">
+      <!-- <div class="bg-gray-800">
         <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
           <h2 class="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">Trusted by over 4600 forward-thinking companies</h2>
           <div class="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
@@ -87,11 +90,11 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
 
     <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="bg-white">
+    <!-- <div class="bg-white">
       <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
         <div class="max-w-3xl mx-auto text-center">
           <h2 class="text-3xl font-extrabold text-gray-900">Our Servises</h2>
@@ -99,7 +102,6 @@
         </div>
         <dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-1 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-3 lg:gap-x-8">
           <div class="flex">
-            <!-- Heroicon name: check -->
             <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
             </svg>
@@ -114,7 +116,6 @@
           </div>
 
           <div class="flex">
-            <!-- Heroicon name: check -->
             <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
             </svg>
@@ -129,7 +130,6 @@
           </div>
 
           <div class="flex">
-            <!-- Heroicon name: check -->
             <svg class="flex-shrink-0 h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
             </svg>
@@ -144,11 +144,11 @@
           </div>
         </dl>
       </div>
-    </div>
+    </div> -->
 
 
     <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="bg-gray-100">
+    <!-- <div class="bg-gray-100">
       <div class="max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:px-8">
         <div class="max-w-lg mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8">
           <div>
@@ -163,7 +163,6 @@
             <div class="mt-9">
               <div class="flex">
                 <div class="flex-shrink-0">
-                  <!-- Heroicon name: phone -->
                   <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
@@ -179,7 +178,6 @@
               </div>
               <div class="mt-6 flex">
                 <div class="flex-shrink-0">
-                  <!-- Heroicon name: mail -->
                   <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
@@ -204,7 +202,6 @@
             <div class="mt-9">
               <div class="flex">
                 <div class="flex-shrink-0">
-                  <!-- Heroicon name: phone -->
                   <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
@@ -220,7 +217,6 @@
               </div>
               <div class="mt-6 flex">
                 <div class="flex-shrink-0">
-                  <!-- Heroicon name: mail -->
                   <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
@@ -235,7 +231,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
 
 
 
